216edd58a992e6241d0ac3359b5bc81a3c0ff3ca
oppara <oppara@oppara.tv>
2016-05-04 12:01:58 +0900

    Step-03 use node

--- javascript/frontend-testing/test/calc-util-spec.js
+++ javascript/frontend-testing/test/calc-util-spec.js
@@ -1,13 +1,15 @@
 //
+var calcUtil = require('../src/calc-util.js');
+
 describe('test of add()', function() {
   it('1 + 2 = 3', function() {
-    if (add(1, 2) !== 3) {
+    if (calcUtil.add(1, 2) !== 3) {
       throw new Error('fail');
     }
   });
 
   it('1 + 2 = 4', function() {
-    if (add(1, 2) !== 4) {
+    if (calcUtil.add(1, 2) !== 4) {
       throw new Error('fail');
     }
   });
