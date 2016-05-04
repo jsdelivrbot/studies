294a91559f608e2aa66b3233d92534f61a632e6e
oppara <oppara@oppara.tv>
2016-05-04 12:23:14 +0900

    Step-05 use assert

--- javascript/frontend-testing/test/calc-util-spec.js
+++ javascript/frontend-testing/test/calc-util-spec.js
@@ -1,16 +1,13 @@
 //
+var assert = require('assert');
 var calcUtil = require('../src/calc-util.js');
 
 describe('test of add()', function() {
   it('1 + 2 = 3', function() {
-    if (calcUtil.add(1, 2) !== 3) {
-      throw new Error('fail');
-    }
+    assert(calcUtil.add(1, 2) === 3);
   });
 
   it('1 + 2 = 4', function() {
-    if (calcUtil.add(1, 2) !== 4) {
-      throw new Error('fail');
-    }
+    assert(calcUtil.add(1, 2) === 4);
   });
 });
